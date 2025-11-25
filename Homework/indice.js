527aade381c491b21512aab1494fb6fd071153b7
Nicolas Alexandre da Silva <nicolas19388108@gmail.com>
2025-11-25 09:08:09 -0300

    Add files via upload

---
+++ Homework/indice.js
@@ -0,0 +1,5 @@
+// Array de número com índice do maior e menor valor
+
+let numeros = [6,2,5,7,12,9]
+console.log(numeros[4])
+console.log(numeros[1])
---
+++ Homework/mdc.js
@@ -0,0 +1,16 @@
+// Máximo Divisor Comum entre dois números
+
+let a = 32, b = 48
+console.log(a/2, b/2)
+let c = 16, d = 24
+console.log(c/2, d/2)
+let e = 8, f = 12
+console.log(e/2, f/2)
+let g = 4, h = 6
+console.log(g/2, h/2)
+let i = 2, j = 3
+console.log(i/2)
+console.log(j/3)
+
+const mdc = (w, x, y, z) => w * x * y * z
+console.log(mdc(2,2,2,2))
