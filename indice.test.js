9c818c1e4eac31efe3aa8aaa08f13d40234e541b
Nicolas Alexandre da Silva <nicolas19388108@gmail.com>
2025-11-28 11:21:05 -0300

    Add files via upload

---
+++ indice.test.js
@@ -0,0 +1,8 @@
+const {numeros} = require('./indice')
+
+describe('indice', () => {
+    it('posição do indice', () => {
+        expect(numeros([4])).toBe(12)
+        expect(numeros([1])).toBe(2)
+    });
+});
---
+++ mdc.test.js
@@ -0,0 +1,19 @@
+const {divisao} = require('./mdc')
+const {quebrado} = require('./mdc')
+const {multiplicaçao} = require('./mdc')
+
+describe('mdc', () => {
+    it('divisao', () => {
+        expect(divisao(32,48)).toBe(16,24)
+        expect(divisao(16,24)).toBe(8,12)
+        expect(divisao(8,12)).toBe(4,6)
+        expect(divisao(4,6)).toBe(2,3)
+        expect(divisao(2)).toBe(1)
+        expect(quebrado(3)).toBe(1)
+    });
+    it('multiplicaçao', () => {
+        expect(multiplicaçao(2)).toBe(4)
+        expect(multiplicaçao(4)).toBe(8)
+        expect(multiplicaçao(8)).toBe(16)
+    });
+});
