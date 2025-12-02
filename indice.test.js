f2cd190b270702c262a5d8ab844e8059f73056be
Nicolas Alexandre da Silva <nicolas19388108@gmail.com>
2025-12-02 11:01:53 -0300

    Update indice.test.js

--- indice.test.js
+++ indice.test.js
@@ -1,8 +1,9 @@
-const {numeros} = require('./indice')
+const {numeros} = require('./Homework/indice')
 
 describe('indice', () => {
     it('posição do indice', () => {
         expect(numeros([4])).toBe(12)
         expect(numeros([1])).toBe(2)
     });
+
 });
